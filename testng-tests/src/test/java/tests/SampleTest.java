777b47ba47056d0fadbb3b290f1123d74e50a3bb
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2019-06-11 21:47:01 +0300

    Add check that cookie button gone after click

--- testng-tests/src/test/java/tests/SampleTest.java
+++ testng-tests/src/test/java/tests/SampleTest.java
@@ -9,7 +9,8 @@ import org.testng.annotations.Test;
 public class SampleTest extends TestBase {
     @Test(description =  "Removing cookie button after opening url")
     public void removeCookies() {
-    wd.findElement(By.cssSelector("button[class='js-cookie-accept button button_blue4 button_full2']")).click();
+        wd.findElement(By.cssSelector("button[class='js-cookie-accept button button_blue4 button_full2']")).click();
+        wd.findElements(By.cssSelector("button[class='js-cookie-accept button button_blue4 button_full2']")).isEmpty();
     }
 
 }
