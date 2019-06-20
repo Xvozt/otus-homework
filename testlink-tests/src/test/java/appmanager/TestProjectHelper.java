950d9160909365e29dca58fe6dd45c5b8263504c
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2019-06-20 23:55:41 +0300

    Creat fillData and submitCreation methods

--- testlink-tests/src/test/java/appmanager/TestProjectHelper.java
+++ testlink-tests/src/test/java/appmanager/TestProjectHelper.java
@@ -15,7 +15,32 @@ public class TestProjectHelper extends HelperBase {
         click(By.cssSelector("#create"));
     }
 
-    public void create() {
+    private void fillTestProjectInfo(String projectName, String projectPrefix, String projectDescription) {
+        switchToDefaultContent();
+        switchToMainFrame();
+        type(By.xpath("//input[@name='tprojectName']"), projectName);
+        type(By.xpath("//input[@name='tcasePrefix']"), projectPrefix);
+        switchToFirstTextInputField();
+        type(By.xpath("//body[@class='cke_editable cke_editable_themed cke_contents_ltr cke_show_borders']"), projectDescription);
+    }
+
+    private void submitTestProjectCreation() {
+        switchToParentFrame();
+        click(By.xpath("//input[@name='doActionButton']"));
+    }
+
+    public void create(String projectName, String projectPrefix, String projectDescription) {
         initProjectCreation();
+        fillTestProjectInfo(projectName, projectPrefix, projectDescription);
+        submitTestProjectCreation();
+    }
+
+    public void delete(String projectName) {
+        switchToDefaultContent();
+        switchToMainFrame();
+        //click on button
+        //switch to alert window
+        //press yes
+
     }
 }
--- testlink-tests/src/test/java/tests/CreateTestProject.java
+++ testlink-tests/src/test/java/tests/CreateTestProject.java
@@ -8,7 +8,11 @@ public class CreateTestProject extends TestBase {
     public void createTestProject() {
         app.goTo().homePage();
         app.goTo().testProjectManagementPage();
-        app.testProject().create();
+        app.testProject().create("Homework4", "hw04", "Some test project for homework 04");
+        app.goTo().homePage();
+        app.goTo().testProjectManagementPage();
+        //assert name, description, public flag
+        app.testProject().delete("Homework4");
     }
 
 }
